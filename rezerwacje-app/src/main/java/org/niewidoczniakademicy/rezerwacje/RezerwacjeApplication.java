59ff2ad48e416c4d09711a7a0576bb6767c14fb9
Tomasz Załęski <tomek30000@gmail.com>
2020-04-02 22:06:49 +0200

    Initial Spring Boot Application

---
+++ rezerwacje-app/src/main/java/org/niewidoczniakademicy/rezerwacje/RezerwacjeApplication.java
@@ -0,0 +1,13 @@
+package org.niewidoczniakademicy.rezerwacje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezerwacjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezerwacjeApplication.class, args);
+	}
+
+}
---
+++ rezerwacje-app/src/test/java/org/niewidoczniakademicy/rezerwacje/RezerwacjeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.niewidoczniakademicy.rezerwacje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezerwacjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
