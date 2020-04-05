7313b2a73857401def47d51ac4cc66ea529d0685
majchrow <dmajchrowski6@gmail.com>
2020-04-05 13:34:13 +0200

    refactor starting script
    remove adding user for starting datbase
    move credentials to .env files
    add proper .env checking in run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-if ! [[ -f ".env" ]] 
+ENV_FILE=".env"
+ENV_EXAMPLE_FILE=".example.env"
+
+if ! [[ -f ${ENV_FILE} ]]
 then
-    echo "Creating .env file"
-    cp .example.env .env
+    echo "Creating ${ENV_FILE} file"
+    cp ${ENV_EXAMPLE_FILE} ${ENV_FILE}
+else
+	required=$(awk -F '=' '{if($2) print $1}' ${ENV_EXAMPLE_FILE} | xargs)
+	current=$(awk -F '=' '{if($2) print $1}' ${ENV_FILE} | xargs)
+	for variable in ${required}
+	do
+		if [[ ! " ${current[@]} " =~ " ${variable} " ]]; then
+			echo >&2 "${variable} NOT FOUND IN ${ENV_FILE}, delete it to use ${ENV_EXAMPLE_FILE} or fix missing fields";
+			exit 1;
+		fi
+	done
 fi
 
 mkdir -p rezerwacje-db/data
-
-export CURRENT_UID=$(id -u):$(id -g)
 docker-compose up "$@"
-
