1d81f3f3d99d1901b8548dabb1a73dc50e05cb20
jfoder <jaroslaw.foder@gmail.com>
2020-04-09 23:10:53 +0200

    [NIAK-20] Add necessary all args constructor for RecruitmentPeriod

--- rezerwacje-app/src/main/java/org/niewidoczniakademicy/rezerwacje/core/model/RecruitmentPeriod.java
+++ rezerwacje-app/src/main/java/org/niewidoczniakademicy/rezerwacje/core/model/RecruitmentPeriod.java
@@ -1,5 +1,6 @@
 package org.niewidoczniakademicy.rezerwacje.core.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 @Entity
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class RecruitmentPeriod {
 
     @Id
