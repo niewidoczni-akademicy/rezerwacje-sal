4d5bd2854746d0083cb818782f87daf62c545627
jakubowiczish <jakub77plotnikowski@gmail.com>
2020-04-10 00:13:12 +0200

    [NIAK-16] Add User database model class.

---
+++ rezerwacje-app/src/main/java/org/niewidoczniakademicy/rezerwacje/core/model/database/User.java
@@ -0,0 +1,34 @@
+package org.niewidoczniakademicy.rezerwacje.core.model.database;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+@Data
+@Table
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private String firstName;
+
+    @NonNull
+    private String lastName;
+
+    @NonNull
+    private String emailAddress;
+
+    @NonNull
+    private String phoneNumber;
+
+    @NonNull
+    private ZonedDateTime additionTime;
+}
