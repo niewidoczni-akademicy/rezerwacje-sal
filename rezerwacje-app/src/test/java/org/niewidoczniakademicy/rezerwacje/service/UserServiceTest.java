d8e405e5f1b4eef0d206dd64d6272f02880f7f94
jakubowiczish <jakub77plotnikowski@gmail.com>
2020-04-15 20:57:01 +0200

    [LIQUIBASE] Basis of liquibase

---
+++ rezerwacje-app/src/test/java/org/niewidoczniakademicy/rezerwacje/service/UserServiceTest.java
@@ -0,0 +1,16 @@
+package org.niewidoczniakademicy.rezerwacje.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public should
+
+}
