512dad782bf82d95e91b33f8e12c1312afc34ca9
Jolanta Gluza <jolanta.gluza@gmail.com>
2020-04-25 15:06:52 +0200

    Modified user form and added validation

--- rezerwacje-front/src/components/Users/UserForm.js
+++ rezerwacje-front/src/components/Users/UserForm.js
@@ -1,98 +1,29 @@
-import React, { Component } from 'react'
-import FormUserDetails from './FormUserDetails'
-import Confirm from './Confirm'
+import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { Grid } from '@material-ui/core';
 
+import UserDetails from './UserDetails';
 
+const UserForm = () => {
 
-export class UserForm extends Component {
-    state = {
-        step: 1,
-        firstName: '',
-        lastName: '',
-        email: '',
-        phone: '',
-        role: '',
-        login: '',
-        password: '',
-        repeatPassword: ''
-        
-    }
+  return (
+    <div> 
+      <Grid
+        container
+        spacing={4}
+      >
+        <Grid
+          item
+          lg={4}
+          md={6}
+          xl={4}
+          xs={12}
+        >
+          <UserDetails />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
-    nextStep = () => {
-        const { step } = this.state;
-        this.setState({
-            step: step + 1
-        });
-    }
-
-    prevStep = () => {
-        const { step } = this.state;
-        this.setState({
-            step: step - 1
-        });
-    }
-
-    handleChange = input => e => {
-        this.setState({[input]: e.target.value})
-    }
-
-    handleAdd = (firstName, lastName, email, phone, login, password) => {
-        fetch("/api/system-user", {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ userUniqueId: login, firstName: firstName, lastName: lastName, emailAddress: email, phoneNumber: phone, 
-        login: login,  password: password}),
-          }).then(
-            function (res) {
-              if (res.ok) {
-                alert("Użytkownik został dodany do bazy.");
-              } else if (res.status === 400) {
-                alert("Wystąpił błąd.");
-              }
-            },
-            function (e) {
-              alert("Wystąpił błąd.");
-            }
-          );
-    }
-
-    render() {
-        const { step } = this.state
-        const { firstName, lastName, email, phone, role, login, password, resetPassword } = this.state
-        const values = { firstName, lastName, email, phone, role, login, password, resetPassword}
-
-        switch (step) {
-            case 1:
-                return (
-                    <FormUserDetails
-                        nextStep={this.nextStep}
-                        handleChange={this.handleChange}
-                        values={values}
-                    />
-                )
-            case 2:
-                return(<Confirm
-                        nextStep={this.nextStep}
-                        prevStep={this.prevStep}
-                        values={values}
-                        />)
-            case 3:
-                this.handleAdd(firstName, lastName, email, phone, login, password)
-                this.setState({step: 1, firstName: '', 
-                    lastName: '', email: '', 
-                    phone: '', role: '', 
-                    login: '', password: '', 
-                    repeatPassword: ''})
-                return (<UserForm/>)
-        
-        }
-
-        return (
-            <div>
-                
-            </div>
-        )
-    }
-}
-
-export default UserForm
+export default UserForm;
---
+++ rezerwacje-front/src/components/Users/useForm.js
@@ -0,0 +1,37 @@
+//custom react hook for forms
+import { useState, useEffect } from "react";
+
+const useForm = (initState, callback, validate) => {
+  const [values, setValues] = useState(initState);
+  const [errors, setErrors] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setValues({
+      ...values,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setErrors(validate(values));
+    setIsSubmitting(true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && isSubmitting) {
+      callback();
+    }
+  }, [errors]);
+
+  return {
+    handleChange,
+    handleSubmit,
+    values,
+    errors
+  };
+};
+
+export default useForm;
