514ed593706daa21acbf54ac3f28ef62f0b19538
BluValor <37878700+BluValor@users.noreply.github.com>
2020-04-26 14:28:06 +0200

    Fix further pull request issues

--- rezerwacje-front/src/App.jsx
+++ rezerwacje-front/src/App.jsx
@@ -1,40 +1,35 @@
 import React from 'react';
-import { Switch, Redirect, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/styles';
 
 import theme from './theme';
 import './App.scss';
 
-import { Main as MainLayout } from './layouts';
+import { Standard as StandardLayout } from './layouts';
 import Routes from './common'
 
-const App = () => {
-
-  const MainRoutes = () => {
-    return (
-      <Switch>
-        {
-          Routes.map(route => 
-            <Route 
-              key={route.title} 
-              exact 
-              path={route.href} 
-              render={() => route.view}
-            />)
-        }
-      </Switch>
-    );
-  };
-
-  return (
-    <ThemeProvider theme={theme}>
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <StandardLayout>
       <Route 
         exact
         path={Routes.map(route => route.href)}
-        render={() => <MainLayout children={<MainRoutes/>}/>} 
+        render={() =>       
+          <Switch>
+            {
+              Routes.map(route => 
+                <Route 
+                  key={route.title} 
+                  exact 
+                  path={route.href} 
+                  render={() => route.view}
+                />)
+            }
+          </Switch> 
+        } 
       />
-    </ThemeProvider>
-  );
-}
+    </StandardLayout>
+  </ThemeProvider>
+)
 
 export default App;
