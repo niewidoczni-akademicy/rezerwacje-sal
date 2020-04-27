7a95331e795f69c4d34a875386a81e39ed0108f0
majchrow <dmajchrowski6@gmail.com>
2020-04-28 00:40:56 +0200

    [NIAK-50] Structurize RestControllers error responses

--- rezerwacje-app/src/main/java/org/niewidoczniakademicy/rezerwacje/controller/exception/RestControllerExceptionHandler.java
+++ rezerwacje-app/src/main/java/org/niewidoczniakademicy/rezerwacje/controller/exception/RestControllerExceptionHandler.java
@@ -6,9 +6,10 @@ import org.niewidoczniakademicy.rezerwacje.service.exception.InvalidEmailAddress
 import org.niewidoczniakademicy.rezerwacje.service.exception.UserNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Arrays;
@@ -18,7 +19,9 @@ import java.util.Arrays;
 public class RestControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({UserNotFoundException.class})
-    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException e) {
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleUserNotFoundException(UserNotFoundException e) {
         final ErrorResponse errorResponse = ErrorResponse.builder()
                 .message(e.getMessage())
                 .status(HttpStatus.NOT_FOUND)
@@ -27,13 +30,13 @@ public class RestControllerExceptionHandler extends ResponseEntityExceptionHandl
         log.warn(e.getMessage());
         log.warn(Arrays.toString(e.getStackTrace()));
 
-        return ResponseEntity
-                .status(errorResponse.getStatus())
-                .body(errorResponse);
+        return errorResponse;
     }
 
     @ExceptionHandler({InvalidEmailAddressException.class})
-    public ResponseEntity<ErrorResponse> handleInvalidEmailAddressException(InvalidEmailAddressException e) {
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleInvalidEmailAddressException(InvalidEmailAddressException e) {
         final ErrorResponse errorResponse = ErrorResponse.builder()
                 .message(e.getMessage())
                 .status(HttpStatus.BAD_REQUEST)
@@ -42,13 +45,14 @@ public class RestControllerExceptionHandler extends ResponseEntityExceptionHandl
         log.warn(e.getMessage());
         log.warn(Arrays.toString(e.getStackTrace()));
 
-        return ResponseEntity
-                .status(errorResponse.getStatus())
-                .body(errorResponse);
+        return errorResponse;
+
     }
 
     @ExceptionHandler({DataIntegrityViolationException.class})
-    public ResponseEntity<ErrorResponse> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleDataIntegrityViolationException(DataIntegrityViolationException e) {
         final ErrorResponse errorResponse = ErrorResponse.builder()
                 .message(e.getMostSpecificCause().getMessage())
                 .status(HttpStatus.BAD_REQUEST)
@@ -57,9 +61,7 @@ public class RestControllerExceptionHandler extends ResponseEntityExceptionHandl
         log.warn(e.getMessage());
         log.warn(Arrays.toString(e.getStackTrace()));
 
-        return ResponseEntity
-                .status(errorResponse.getStatus())
-                .body(errorResponse);
+        return errorResponse;
     }
 
 }
