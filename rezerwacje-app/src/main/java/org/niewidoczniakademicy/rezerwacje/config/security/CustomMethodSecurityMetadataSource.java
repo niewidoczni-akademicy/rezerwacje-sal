2e71b95a3219bd9544049f0114df4ad98b30cb6f
Tomasz Załęski <tomek30000@gmail.com>
2020-05-09 23:56:59 +0200

    [NIAK-55] Added Spring Security configuration

---
+++ rezerwacje-app/src/main/java/org/niewidoczniakademicy/rezerwacje/config/security/CustomMethodSecurityMetadataSource.java
@@ -0,0 +1,41 @@
+package org.niewidoczniakademicy.rezerwacje.config.security;
+
+import org.springframework.core.annotation.MergedAnnotations;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.method.AbstractFallbackMethodSecurityMetadataSource;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.List;
+
+import static org.springframework.security.access.annotation.Jsr250SecurityConfig.DENY_ALL_ATTRIBUTE;
+
+public final class CustomMethodSecurityMetadataSource extends AbstractFallbackMethodSecurityMetadataSource {
+
+    @Override
+    protected Collection<ConfigAttribute> findAttributes(Method method, Class<?> targetClass) {
+        MergedAnnotations methodAnnotations = MergedAnnotations.from(method);
+        if (targetClass.getPackageName().startsWith("org.niewidoczniakademicy.rezerwacje")
+                && MergedAnnotations.from(method.getDeclaringClass()).isPresent(Controller.class)
+                && methodAnnotations.isPresent(RequestMapping.class)
+                && !(methodAnnotations.isPresent(PreAuthorize.class) || methodAnnotations.isPresent(Secured.class))) {
+            logger.warn("Unsecured " + method.toGenericString());
+            return List.of(DENY_ALL_ATTRIBUTE);
+        }
+        return null;
+    }
+
+    @Override
+    protected Collection<ConfigAttribute> findAttributes(Class<?> clazz) {
+        return null;
+    }
+
+    @Override
+    public Collection<ConfigAttribute> getAllConfigAttributes() {
+        return null;
+    }
+}
