4606ddfaa642b53d4b33468109aff9e871214b30
BluValor <37878700+BluValor@users.noreply.github.com>
2020-05-18 21:41:59 +0200

    Cleanup Schedule constants

--- rezerwacje-front/src/views/Schedule/components/Calendar/constants.js
+++ rezerwacje-front/src/views/Schedule/components/Calendar/constants.js
@@ -1,19 +1,3 @@
-export const START_YEAR = 2020
-export const NUM_OF_YEARS = 3
-export const MONTHS_PER_YEAR = 12
-export const QUARTERS_PER_YEAR = 4
-export const MONTHS_PER_QUARTER = 3
-export const NUM_OF_MONTHS = NUM_OF_YEARS * MONTHS_PER_YEAR
-export const MAX_TRACK_START_GAP = 10
-export const MAX_ELEMENT_GAP = 5
-export const MAX_MONTH_SPAN = 8
-export const MIN_MONTH_SPAN = 2
-export const NUM_OF_TRACKS = 5
-export const MAX_NUM_OF_SUBTRACKS = 2
-
-
-export const START_DATE = '2020-04-28'
-export const END_DATE = '2020-05-03'
 export const WEEKDAY_NAMES = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 export const MONTH_NAMES = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 export const RELATION_COLOR = {
