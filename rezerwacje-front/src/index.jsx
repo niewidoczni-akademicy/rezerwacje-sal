f33ddf2f2a92bd6d46d290ce8479ef3ecb33dfe1
Tomasz Załęski <tomek30000@gmail.com>
2020-05-19 23:21:53 +0200

    [NIAK-62] Added user reducer

--- rezerwacje-front/src/index.jsx
+++ rezerwacje-front/src/index.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import * as serviceWorker from './serviceWorker';
-import App from './App';
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+
+import * as serviceWorker from "./serviceWorker";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ rezerwacje-front/src/redux/store.js
@@ -0,0 +1,10 @@
+import logger from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
+
+import rootReducer from "./root-reducer";
+
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") middlewares.push(logger);
+
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
