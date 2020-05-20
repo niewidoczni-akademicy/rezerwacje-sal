5c096fd218b8e4b18f19468d42d36e12bbf9e8d6
Tomasz Załęski <tomek30000@gmail.com>
2020-05-20 02:56:07 +0200

    [NIAK-62] Missing routes fix

--- rezerwacje-front/src/redux/routes/routes.data.jsx
+++ rezerwacje-front/src/redux/routes/routes.data.jsx
@@ -1,51 +1,86 @@
-import React from 'react';
-import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
-import HomeIcon from '@material-ui/icons/Home';
-import PersonIcon from '@material-ui/icons/Person';
-import TodayIcon from '@material-ui/icons/Today';
-import {
-  Home as HomeView,
-  Rooms as RoomsView,
-  Users as UsersView,
-  Exams as ExamsView
-} from '../../views';
+import React from "react";
+import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';	
+import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';	
+import HomeIcon from '@material-ui/icons/Home';	
+import PersonIcon from '@material-ui/icons/Person';	
+import TodayIcon from '@material-ui/icons/Today';	
+import SchoolIcon from '@material-ui/icons/School';	
+import ScheduleIcon from '@material-ui/icons/CalendarToday';	
+import {	
+  Home as HomeView,	
+  Rooms as RoomsView,	
+  Courses as CoursesView,	
+  Users as UsersView,	
+  Exams as ExamsView,	
+  Recruitments as RecruitmentsView,	
+  UserCourses as UserCoursesView,	
+  Schedule as ScheduleView	
+} from 'views';
 
 const ROUTES_DATA = [
   {
-    title: 'Home',
-    href: '/',
+    title: "Home",
+    href: "/",
     icon: <HomeIcon />,
     view: <HomeView />,
     roles: ["STANDARD", "SUPERVISOR", "ADMINISTRATOR"],
   },
   {
-    title: 'Rooms',
-    href: '/rooms',
+    title: "Rooms",
+    href: "/rooms",
     icon: <MeetingRoomIcon />,
     view: <RoomsView />,
     roles: ["STANDARD", "SUPERVISOR", "ADMINISTRATOR"],
   },
   {
-    title: 'Exams',
-    href: '/exams',
-    icon: <TodayIcon/>,
-    view: <ExamsView/>,
+    title: "Courses",
+    href: "/courses",
+    icon: <FormatListBulletedIcon />,
+    view: <CoursesView />,
     roles: ["STANDARD", "SUPERVISOR", "ADMINISTRATOR"],
   },
   {
-    title: 'Users',
-    href: '/users/add',
+    title: "Exams",
+    href: "/exams",
+    icon: <TodayIcon />,
+    view: <ExamsView />,
+    roles: ["STANDARD", "SUPERVISOR", "ADMINISTRATOR"],
+  },
+  {
+    title: "Recruitments",
+    href: "/recruitments",
+    icon: <SchoolIcon />,
+    view: <RecruitmentsView />,
+    roles: ["STANDARD", "SUPERVISOR", "ADMINISTRATOR"],
+  },
+  {
+    title: "Users",
+    href: "/users/add",
     icon: <PersonIcon />,
     view: <UsersView />,
     roles: ["STANDARD", "SUPERVISOR", "ADMINISTRATOR"],
   },
   {
-    title: 'Login',
-    href: '/login',
+    title: "Schedule",
+    href: "/schedule",
+    icon: <ScheduleIcon />,
+    view: <ScheduleView />,
+    roles: ["STANDARD", "SUPERVISOR", "ADMINISTRATOR"],
+  },
+  {
+    title: "Standard Users",
+    href: "/users/standard",
+    icon: <PersonIcon />,
+    view: <UserCoursesView />,
+    roles: ["STANDARD", "SUPERVISOR", "ADMINISTRATOR"],
+  },
+  {
+    title: "Login",
+    href: "/login",
     icon: <PersonIcon />, //TODO
     view: <UsersView />,
     roles: ["ANON"],
-  }
+  },
 ];
 
 export default ROUTES_DATA;
